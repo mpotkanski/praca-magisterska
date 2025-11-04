49aa5ca55b1a9a8d093346eb82c08354aa6c021a
mpotkanski <122682224+mpotkanski@users.noreply.github.com>
2025-11-04 14:01:52 +0100

    Add files via upload

---
+++ Concat2.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class Concat2 {
+
+    public static void main(String[] args) {
+        int[] array1 = new int[10000000]; //000000
+        int[] array2 = new int[10000000];
+
+        for (int i=0; i < 10000000;i++) {
+            array1[i] = i;
+            array2[i] = i + 10000000;
+        }	
+	
+        int aLen = array1.length;
+        int bLen = array2.length;
+        
+        // Start measuring execution time
+        long startTime = System.nanoTime();
+        
+        int[] result = new int[aLen];
+
+        for (int i = 0; i < aLen; i++) {
+            result[i] = array1[i] + array2[i];
+        }
+
+        // Stop measuring execution time
+        long endTime = System.nanoTime();
+
+        // Calculate the execution time in milliseconds
+        long executionTime
+            = (endTime - startTime);
+
+        double seconds = (double)executionTime / 1_000_000_000.0;
+        System.out.println(seconds + " s");
+    }
+}
