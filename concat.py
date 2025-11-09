627d2c8a59fe77c1192e9b9f5e8a4ee86b636de3
mpotkanski <122682224+mpotkanski@users.noreply.github.com>
2025-11-09 12:30:12 +0100

    Update concat.py

--- concat.py
+++ concat.py
@@ -2,23 +2,23 @@ import numpy as np
 from time import perf_counter_ns
 
 # Define two Python lists 
-py_list1 = list(range(10000000)) 
-py_list2 = list(range(10000000, 20000000))  
+py_list1 = list(range(10_000_000)) 
+py_list2 = list(range(10_000_000, 20_000_000))  
 
 # Define two Numpy arrays 
-np_array1 = np.arange(10000000) 
-np_array2 = np.arange(10000000, 20000000)  
+np_array1 = np.arange(100_00_000) 
+np_array2 = np.arange(100_00_000, 20_000_000)  
 
 # Adding Python lists 
 t1_start = perf_counter_ns()
 result_list = [a + b for a, b in zip(py_list1, py_list2)] 
 t1_stop = perf_counter_ns()
 
-print((t1_stop-t1_start) / 1000000000, 's')
+print((t1_stop-t1_start) / 1_000_000_000, 's')
 
 # Adding Numpy arrays 
 t2_start = perf_counter_ns()
 result_array = np_array1 + np_array2 
 t2_stop = perf_counter_ns()
 
-print((t2_stop-t2_start) / 1000000000, 's')
+print((t2_stop-t2_start) / 1_000_000_000, 's')
