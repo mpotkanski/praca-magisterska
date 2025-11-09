c835b9f7d516f25aa07500734ff2814c81a5ea86
mpotkanski <122682224+mpotkanski@users.noreply.github.com>
2025-11-09 12:28:57 +0100

    Update addition4.py

--- addition4.py
+++ addition4.py
@@ -3,8 +3,8 @@ import sys
 
 def my_function():
   sum = 0
-  r = 2000**2000
-  for i in range(100**100, 100**100 + 100000000):
+  r = 2_000**2_000
+  for i in range(100**100, 100**100 + 100_000_000):
     sum = sum + r
 		
 # Start the stopwatch / counter
@@ -15,5 +15,5 @@ my_function()
 # Stop the stopwatch / counter
 t1_stop = perf_counter_ns()
 
-print((t1_stop-t1_start) / 1000000000, 's')
+print((t1_stop-t1_start) / 1_000_000_000, 's')
 
