04c6681d628c9ed474554bbd6bfca1ec4828d33b
mpotkanski <122682224+mpotkanski@users.noreply.github.com>
2025-11-09 12:29:35 +0100

    Update empty2.py

--- empty2.py
+++ empty2.py
@@ -3,11 +3,11 @@ from time import perf_counter_ns
 # Start the stopwatch / counter
 t1_start = perf_counter_ns()
 
-for i in range(100000000):
+for i in range(100_000_000):
   pass
 
 # Stop the stopwatch / counter
 t1_stop = perf_counter_ns()
 
-print((t1_stop-t1_start) / 1000000000, 's')
+print((t1_stop-t1_start) / 1_000_000_000, 's')
 
